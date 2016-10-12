62691d398de0b6bce011c43ef3e454b6c420bcee
Martynas Kadiša <candylifter@gmail.com>
2016-10-13 00:21:20 +0300

    Add functionality

--- src/js/editor.js
+++ src/js/editor.js
@@ -6,31 +6,106 @@ export class Editor {
   constructor () {
     this.state = {
       regions: document.querySelectorAll('[data-editable]'),
+      prevContent: undefined,
+      nextContent: undefined,
       isEditing: false,
       isPosting: false
     }
+
+    this.enableEdit = this.enableEdit.bind(this)
+    this.disableEdit = this.disableEdit.bind(this)
+    this.handleFabClick = this.handleFabClick.bind(this)
   }
 
-  enableEdit (element) {
-    element.setAttribute('contenteditable', true)
+  handleSave () {
+    let { prevContent, nextContent } = this.state
+
+    let changedContent = nextContent.filter((next, index) => {
+      return next.content !== prevContent[index].content
+    })
+
+    console.log(JSON.stringify(changedContent))
   }
 
-  disableEdit (element) {
-    element.setAttribute('contenteditable', false)
+  handleFabClick (e) {
+    let { isEditing } = this.state
+
+    if (isEditing) {
+      e.target.classList.remove('editor-fab--save')
+      this.disableEdit()
+      this.updateNextContent()
+      this.handleSave()
+    } else {
+      e.target.classList.add('editor-fab--save')
+      this.enableEdit()
+      this.updatePrevContent()
+    }
   }
 
-  createFab () {
+  updatePrevContent () {
+    let { regions } = this.state
+
+    let prev = []
+
+    for (var i = 0; i < regions.length; i++) {
+      let prevContent = {
+        name: regions[i].dataset.name,
+        content: regions[i].innerText
+      }
+
+      prev.push(prevContent)
+    }
+
+    this.state.prevContent = prev
+  }
+
+  updateNextContent () {
+    let { regions } = this.state
+
+    let next = []
+
+    for (var i = 0; i < regions.length; i++) {
+      let nextContent = {
+        name: regions[i].dataset.name,
+        content: regions[i].innerText
+      }
+
+      next.push(nextContent)
+    }
+
+    this.state.nextContent = next
+  }
+
+  enableEdit () {
+    let { regions } = this.state
+
+    for (var i = 0; i < regions.length; i++) {
+      regions[i].setAttribute('contenteditable', true)
+    }
+
+    this.state.isEditing = true
+  }
+
+  disableEdit () {
+    let { regions } = this.state
+
+    for (var i = 0; i < regions.length; i++) {
+      regions[i].removeAttribute('contenteditable')
+    }
+
+    this.state.isEditing = false
+  }
+
+  addFab () {
     let fab = document.createElement('div')
     fab.classList.add('editor-fab')
+    fab.addEventListener('click', this.handleFabClick, false)
 
     document.body.appendChild(fab)
   }
 
   init () {
-    let { regions } = this.state
-
-    this.enableEdit(regions[0])
-    this.createFab()
+    this.addFab()
   }
 }
 
