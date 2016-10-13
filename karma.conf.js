ad49d8c2ccde71ae4181e461d0853690e35178aa
Martynas Kadiša <candylifter@gmail.com>
2016-10-13 12:55:53 +0300

    Setting up karma with travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,32 @@
 var webpackConfig = require('./webpack.config.js')
 
-module.exports = function (config) {
-  config.set({
-    browsers: ['Chrome'],
-    singleRun: true,
-    frameworks: ['mocha'],
-    files: [
-      'node_modules/jquery/dist/jquery.min.js',
-      'src/tests/**/*.test.js'
-    ],
-    preprocessors: {
-      'src/tests/**/*.test.js': ['webpack', 'sourcemap']
-    },
-    reporters: ['mocha'],
-    client: {
-      mocha: {
-        timeout: '5000'
-      }
-    },
-    webpack: webpackConfig,
-    webpackServer: {
-      noInfo: true
+var configuration = {
+  browsers: ['Chrome'],
+  singleRun: true,
+  frameworks: ['mocha'],
+  files: [
+    'node_modules/jquery/dist/jquery.min.js',
+    'src/tests/**/*.test.js'
+  ],
+  preprocessors: {
+    'src/tests/**/*.test.js': ['webpack', 'sourcemap']
+  },
+  reporters: ['mocha'],
+  client: {
+    mocha: {
+      timeout: '5000'
     }
-  })
+  },
+  webpack: webpackConfig,
+  webpackServer: {
+    noInfo: true
+  }
+}
+
+if (process.env.TRAVIS) {
+  configuration.browsers = ['Chrome_travis.ci']
+}
+
+module.exports = function (config) {
+  config.set(configuration)
 }
